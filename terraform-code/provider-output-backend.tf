cc2f4c21272b13189140dd9859197b7582ff550f
nl-alexmok <64762294+nl-alexmok@users.noreply.github.com>
2021-07-19 12:38:41 +0800

    Update provider-output-backend.tf

--- terraform-code/provider-output-backend.tf
+++ terraform-code/provider-output-backend.tf
@@ -2,6 +2,7 @@ provider "aws" {
 #  shared_credentials_file  = "${var.cred-file}" # we don't use it in CodeBuild
   profile    = "${var.profile}"
   region     = "${var.region}"
+  version    = "v2.70.0"
 }
 
 /* THIS SYNTAX IS NOT WORKING */
