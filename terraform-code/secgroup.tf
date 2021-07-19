bf1476bc56101efd78622b30561b7c9736719269
nl-alexmok <64762294+nl-alexmok@users.noreply.github.com>
2021-07-19 14:15:40 +0800

    Update secgroup.tf

--- terraform-code/secgroup.tf
+++ terraform-code/secgroup.tf
@@ -11,6 +11,26 @@ resource "aws_security_group" "Demo" {
     cidr_blocks = ["0.0.0.0/0"]
    }
    ingress {
+     from_port   = 80
+     to_port     = 80 # All outbound traffic
+     protocol    = "TCP"
+     cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "Demo-2" {
+  name = "Demo-SG-v2"
+  tags {
+        Name = "Demo-SG-v2"
+  }
+  description = "Demo SG v2"
+  egress {
+    from_port   = 0
+    to_port     = 65535 # All outbound traffic
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+   ingress {
      from_port   = 443
      to_port     = 443 # All outbound traffic
      protocol    = "TCP"
